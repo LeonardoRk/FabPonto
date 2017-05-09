a7e81946510dcbd157a9059c54ab040df67f9e96
Gustavo Moreira <gustavomn93@gmail.com>
2017-05-09 18:38:42 -0300

    Adding sartup class for authentication

---
+++ FabPonto/App_Start/SartUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+
+namespace FabPonto
+{
+    public class SartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/auth/login")
+            });
+        }
+    }
+}
