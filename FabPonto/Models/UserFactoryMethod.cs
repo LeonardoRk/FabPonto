538cd7a88367f9d99ef77ed505cf1bc2299abff6
Marcelo Cristiano <marceloabk@hotmail.com>
2017-05-09 21:59:53 -0300

    Creating state types of state pattern

---
+++ FabPonto/Models/UserFactoryMethod.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace FabPonto.Models
+{
+    public class UserFactoryMethod
+    {
+        public const int NormalUser = 0;
+        public const int AdminUser = 1;
+
+        public IUser FactoryUser(int userType)
+        {
+            IUser user;
+
+            switch (userType)
+            {
+                case NormalUser:
+                    user = new User();
+                    break;
+                case AdminUser:
+                    user = new Admin();
+                    break;
+                default:
+                    throw new InvalidEnumArgumentException("Invalid user type");
+            }
+
+            return user;
+        }
+
+    }
+}
---
+++ FabPonto/Models/WorkingState.cs
@@ -0,0 +1,10 @@
+﻿namespace FabPonto.Models
+{
+    public class WorkingState:IState
+    {
+        public void ChangeState(AbstractUser user)
+        {
+            user.WorkingState = new NotWorkingState();
+        }
+    }
+}
