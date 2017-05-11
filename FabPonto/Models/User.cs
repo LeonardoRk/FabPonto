9ed14c68a3e64fc2b3f7e1780cd3424854129312
LeonardoRk <leodegolim@yahoo.com.br>
2017-05-11 16:57:37 -0300

    adds classes of iterator

--- FabPonto/Models/User.cs
+++ FabPonto/Models/User.cs
@@ -4,6 +4,14 @@ namespace FabPonto.Models
 {
     public sealed class User:AbstractUser
     {
+        private IIterator workdayIterator = null;
+
+        public override ConcreteIterator CreateIterator()
+        {
+            ConcreteIterator workdaysIterator = new ConcreteIterator(this);
+            return workdaysIterator;
+        }
+
         public User()
         {
             Workdays = new HashSet<Workday>();
