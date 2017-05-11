2dfaf8137ab88d3eab918469ad313fb52e6717ba
Thiago Lima <thiagolima.unb@gmail.com>
2017-05-11 14:24:07 -0300

    creating team class

---
+++ FabPonto/Models/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+namespace FabPonto.Models
+{
+    public class Team
+    {
+        private ArrayList userArray;
+        private String name { get; set; }
+
+        public Team()
+        {
+            userArray = new ArrayList();
+        }
+    }
+}
