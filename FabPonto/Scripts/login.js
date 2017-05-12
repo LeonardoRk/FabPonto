5926049ae620994bb0ca1f4927c10826543c1406
Ygor Galeno <ygor.tgaleno@gmail.com>
2017-05-12 01:14:29 -0300

    Calling modal in home
    Signed-off-by: Marcelo Cristiano <marceloabk@hotmail.com>
    Signed-off-by: Ygor Galeno <ygor.tgaleno@gmail.com>

--- FabPonto/Scripts/login.js
+++ FabPonto/Scripts/login.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    $("#login-modal").modal('show');
+    $(".login").click(function() {
+        show_modal();
+    });
+    $("button[type=submit]").click(function () {
+        $(this).attr("data-dismiss", "modal");
+        hide_modal();
+    });
+
+    function hide_modal() {
+        $("#login-modal").modal('toogle');
+    }
+    function show_modal() {
+        $("#login-modal").modal('show');
+    }
 });
